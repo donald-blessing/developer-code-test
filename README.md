a905842849f805bdea57038b734605d24047caac
Donald Blessing <donaldblessing9@gmail.com>
2023-02-19 18:02:02 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+## Coding Test
+
+Create a RESTFUL API for a contact form that stores its record in the dB
+The fields in the form should include:
+- Name
+- Email
+- Attachment
+- Message
+
+The form should prevent duplicate submission and send email notifications to a value specified in your .env.
+The contact form should allow only PNG, SVG and CSV attachments.
+
+Write unit tests that cover at least one(1) edge case of your choice.
+
+The code should be written and implemented in the Laravel framework
---
+++ app/Http/Resources/ContactResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ContactResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'name' => $this->name,
+            'email' => $this->email,
+            'attachment' => $this->attachment,
+            'message' => $this->message,
+            'created_at' => $this->created_at->format('d-m-Y'),
+            'updated_at' => $this->updated_at->format('d-m-Y'),
+        ];
+    }
+}
